bceae5ca1a77839505897e7ebbfe3b7f87dd7d3a
perix01 <perixcat@gmail.com>
2015-11-12 12:53:19 +0100

    Missatge

---
+++ src/danielPerez/ClasseInicial.java
@@ -0,0 +1,6 @@
+package danielPerez;
+
+public class ClasseInicial {
+	public static final String ATRIBUT_EAC2 = "Valor de l'atribut";
+}
+
---
+++ src/danielPerez/ClasseNova.java
@@ -0,0 +1,5 @@
+package danielPerez;
+
+public class ClasseNova {
+
+}
